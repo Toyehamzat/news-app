51ef42cd8f913e5e0a29f0f0433697727518258e
Toyehamzat <anibiseun@gmail.com>
2023-11-16 18:03:25 +0000

    added usequery

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
-import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AppNavigation from "./src/navigations";
 
+const queryClient = new QueryClient();
+
 export default function App() {
-  return <AppNavigation />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AppNavigation />
+    </QueryClientProvider>
+  );
 }
